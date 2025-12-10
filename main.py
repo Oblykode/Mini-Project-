6f116f14a80b6a187ee68c4516fb6a5ee7e234dd
Anil Tamang <aniltmg871@gmail.com>
2025-12-10 19:30:04 +0545

    Refactor dice game logic and improve user prompts
    Refactor the dice game code for clarity and functionality.

--- main.py
+++ main.py
@@ -1,56 +1,56 @@
 import random
 
 def roll():
-    Min_value = 1 
-    Max_value= 6 
-    roll = random.randint(Min_value , Max_value)
+    min_value = 1
+    max_value = 6
+    return random.randint(min_value, max_value)
 
-    return roll 
-
+# Ask for number of players
 while True:
-    Players = input("Enter the number of player you want 2 - 4: " )
-    if Players.isdigit():
-        Players = int(Players)
-        if 2 <= Players <=4:
+    players = input("Enter the number of players you want (2 - 4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
             break
         else:
-            print("enter the number between 2 - 4: ")
-    else :
-        print("enter a valid number")
+            print("Please enter a number between 2 and 4.")
+    else:
+        print("Please enter a valid number.")
 
-Max_scores = 50
-Players_score = [0 for i in range(Players)]
+target_score = 50
+players_score = [0 for _ in range(players)]
 
-while max(Players_score) < Max_scores:
-    for i in range(Players):
-        print("\n Player" , i + 1 ,"turn has just started \n")
-        Current_score = 0 
+# Main game loop
+while max(players_score) < target_score:
+    for i in range(players):
+        print("\nPlayer", i + 1, "turn has just started.")
+        print("Your total score is:", players_score[i])
+        current_score = 0
+
         while True:
-            Should_roll = input("do you want to roll ? ")
-            if Should_roll.lower() != "y" :
-                break 
-            
-            Value = roll()
-            
-
-            if Value == 1 :
-                print("you rolled" , Value )
-                print("your turn is done")
+            should_roll = input("Do you want to roll? (y/n): ")
+            if should_roll.lower() != "y":
                 break
-            else :
-                Current_score += Value
-                print("you rolled a : ", Value )
-            
-            print("your current score is : " , Current_score)
-        Players_score[i] += Current_score
-        print("your current score is ; ",Players_score[i])
-        break
 
-Max_scores = max(Players_score)
-winning_idx =  Players_score.index(Max_scores)
-print("Player number" ,winning_idx +1 , "is the winner with a score of : " ,Max_scores)
+            value = roll()
+            print("You rolled:", value)
 
+            if value == 1:
+                print("You rolled a 1. Your turn is over and you gain 0 points this turn.")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("Your current turn score is:", current_score)
 
-    
+        players_score[i] += current_score
+        print("Player", i + 1, "total score is now:", players_score[i])
 
+        # If this player has reached or passed the target, end the game
+        if players_score[i] >= target_score:
+            break
 
+# Determine winner
+winning_score = max(players_score)
+winning_idx = players_score.index(winning_score)
+print("\nPlayer number", winning_idx + 1, "is the winner with a score of:", winning_score)
